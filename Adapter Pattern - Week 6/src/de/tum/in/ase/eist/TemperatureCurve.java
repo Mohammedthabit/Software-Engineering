f1f625be602a8716c59d519083a418b353832410
Mohammed Thabit <97237393+Mohammedthabit@users.noreply.github.com>
2023-06-21 00:48:13 +0200

    Added Week 6

---
+++ Adapter Pattern - Week 6/src/de/tum/in/ase/eist/TemperatureCurve.java
@@ -0,0 +1,21 @@
+package de.tum.in.ase.eist;
+
+import java.text.DecimalFormat;
+
+public final class TemperatureCurve {
+
+	private static final int TEMPERATURE_SAMPLES = 15;
+
+	private TemperatureCurve() {
+	}
+
+	public static void main(String[] args) {
+		ThermoInterface thermo = new ThermoAdapter();
+
+		DecimalFormat df = new DecimalFormat("#.#");
+
+		for (int i = 0; i < TEMPERATURE_SAMPLES; i++) {
+			System.out.println("Sample " + (i + 1) + ": " + df.format(thermo.getTempC()));
+		}
+	}
+}
